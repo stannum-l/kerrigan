af84526c4b7fbcb376040bb598b3b8ef0048aff6
Tin Lam <tin@irrational.io>
2017-12-25 13:21:54 -0600

    Add k8s keystone auth dockerfile
    This patch set adds the dockerfile to generate an image that incorporates
    the kubernetes-keystone-authentication plugin outlined in repository [0].
    This patch set also migrates the dockerfile from repository [1].
    
    [0] https://github.com/dims/k8s-keystone-auth
    [1] https://github.com/stannum-l/docker-ksk8sauth
    
    Change-Id: I4ae1b5f3a9ac5b8e063637c881163b5a43595cd6

---
+++ Dockerfiles/k8s-keystone-auth/README.md
@@ -0,0 +1,7 @@
+Kubernetes-Keystone Integration
+===============================
+
+This docker image provides an installation of the Kubernetes-Keystone
+integration provided by this repository:
+
+https://github.com/dims/k8s-keystone-auth
---
+++ Dockerfiles/k8s-keystone-auth/assets/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+set -xe
+
+# Sample syntax to launch the webhook.
+# /usr/local/bin/k8s-keystone-auth \
+#   --tls-cert-file ${TLS_CERT_FILE} \
+#   --tls-private-key-file ${TLS_CERT_PRIVATE_KEY} \
+#   --keystone-policy-file ${K8S_KEYSTONE_POLICY_FILE} \
+#   --keystone-url ${K8S_KEYSTONE_URL}
